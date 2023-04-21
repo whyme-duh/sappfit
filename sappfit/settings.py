1933ae4d95520d156fd2a4c716b515be993ca5a3
whyme-duh <ritikshrestha94@gmail.com>
2023-04-21 22:49:28 +0545

    add all

--- sappfit/settings.py
+++ sappfit/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9ao%m!fonsyg*p%4pc_tjq%!f&-7#^(c%vcsb!bu(vis%h)920'
+SECRET_KEY = os.environ.get("SECRET_KEY_SETTINGS")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -117,10 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 MEDIA_URL = "/media/"
 
-STATIC_ROOT= os.path.join(BASE_DIR,'static')
+
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
